64ffd7fad873b5acf22206287d869b5d80ea9462
Eric Nelson <eric@sandalsoft.com>
2016-12-08 09:42:13 -0600

    - Added Moment.js - Basic parsing Github API ratelimits

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const request = require('needle')
 const GitHubApi = require('github')
+var moment = require('moment')
+// moment().format('mm')
 const log = require('winston')
 log.level = process.env['NODE_LOG_LEVEL'] || 'info'
 
+const githubAPIToken = process.env['GITHUB_API_TOKEN']
 let categorySplitString = '###'
 
 var github = new GitHubApi({
-  debug: true,
+  debug: false,
   protocol: 'https',
   host: 'api.github.com',
   headers: { 'User-Agent': 'sandalsoft/awesome-repodata' },
@@ -15,21 +18,48 @@ var github = new GitHubApi({
   timeout: 2500
 })
 
+github.authenticate({
+  type: 'token',
+  token: githubAPIToken
+})
+
+// info: Rate Limit data: {
+// "resources":{"core":{"limit":5000,"remaining":4994,"reset":1481184442},"search":{"limit":30,"remaining":30,"reset":1481182219},"graphql":{"limit":200,"remaining":200,"reset":1481185759}},"rate":{"limit":5000,"remaining":4994,"reset":1481184442},"meta":{"x-ratelimit-limit":"5000","x-ratelimit-remaining":"4994","x-ratelimit-reset":"1481184442","x-oauth-scopes":"notifications, public_repo, read:org, read:repo_hook, repo:status, repo_deployment","x-github-request-id":"49086F56:4B42:4D2F24:58490BCF","status":"200 OK"}}
+
+github.misc.getRateLimit({}, function (err, res) {
+  if (err) {
+    log.error('Error getting Github Rate Limit')
+    log.error(`\t\t${err}`)
+  } else {
+    log.debug('Ratelimit payload: ' + JSON.stringify(res.resources.core))
+    let limit = res.resources.core.limit
+    let remaining = res.resources.core.remaining
+    let pctLeft = (remaining / limit * 100.00)
+    let timeLeft = moment.unix(res.resources.core.reset).fromNow()
+    log.info(`Rate Limit info!`)
+    log.info(`\t\tLimit: ${limit}`)
+    log.info(`\t\tRemaining: ${remaining}`)
+    log.info(`\t\t${pctLeft}% left`)
+    log.info(`\t\tRests ${timeLeft}`)
+  }
+})
+
 // let repoOwner = 'ikesyo'
 // let repoName = 'Himotoki'
-let readmeUrl = 'https://raw.githubusercontent.com/matteocrippa/awesome-swift/master/README.md'
+let readmeUrl = 'https://raw.githubusercontent.com/sandalsoft/awesome-repodata/master/test/fixtures/readme_fixture.md'
 
 // INSERT COIN
-getReadme(readmeUrl).then(readme => {
+getReadme(readmeUrl)
+.then(readme => {
   let categoryList = parseReadmeIntoCategories(readme)
   categoryList.map(category => {
     let repos = category.split('\n')
     let categoryName = repos[0].replace(/\W/g, '')
     log.debug('CategoryName: ' + categoryName)
-    repos.map(repo => {
-      // log.debug(repo)
+    repos.map(repoLine => {
+      log.debug(repoLine)
       let repoRe = /^\* \[(.*?)\]\(https:\/\/github\.com\/(.+?)\/(.+?)\)/gmi
-      let match = repoRe.exec(repo)
+      let match = repoRe.exec(repoLine)
 
       if (match === null) return
 
@@ -40,6 +70,7 @@ getReadme(readmeUrl).then(readme => {
       .then(numStars => {
         log.debug(`\t\t\t${owner}/${repoName} has ${numStars}`)
       })// then
+  .catch(error => log.error(error))
     })// map
   })// map
 })// then
@@ -57,10 +88,12 @@ function getReadme (readmeUrl) {
     request.get(readmeUrl, function (err, resp) {
       if (err) {
         log.error('Error getting readme from ' + readmeUrl)
-        reject()
+        log.error(`\t\t${err}`)
+        reject(err)
+      } else {
+        // console.log('got resp at : ' + resp.body)
+        resolve(resp.body)
       }
-      // console.log('got resp at : ' + resp.body)
-      resolve(resp.body)
     })// request
   })// promise
 }// getReadme()
@@ -71,18 +104,26 @@ function getNumStarsForRepo (repoOwner, repoName) {
       owner: repoOwner,
       repo: repoName
     }, function (err, response) {
+      if (response !== null || response !== 'undefined') {
+        log.debug('RESPONSE: ' + response)
+      }
+      // if (response.statusCode === 403) {
+      //   log.info('Forbidden headers: ' + response.headers)
+      // }
       if (err) {
         log.error(`Error getting stars for repo ${repoOwner}/${repoName}`)
-        reject()
+        log.error(`\t\t${err}`)
+        process.exit(1)
+        reject(err)
+      } else {
+        resolve(response.length)
       }
-      resolve(response.length)
     })// github.
   })// promise
 }// getNumStarsForRepo()
 
 // Blatantly copy-pasta'd from http://machinesaredigging.com/2014/04/27/binary-insert-how-to-keep-an-array-sorted-as-you-insert-data-in-it/
 function binaryInsert (value, array, startVal, endVal) {
-
   var length = array.length
   var start = typeof startVal !== 'undefined' ? startVal : 0
   var end = typeof endVal !== 'undefined' ? endVal : length - 1 // !! endVal could be 0 don't use || syntax
