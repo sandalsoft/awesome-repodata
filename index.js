5bad857ce1cc52dd5c0e247cdac57899a937be06
Eric Nelson <eric@sandalsoft.com>
2016-12-08 00:53:34 -0600

    - Added test directory and sample readme.md and stargazer fixtures - regex works now

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 const request = require('needle')
 const GitHubApi = require('github')
 const log = require('winston')
-log.level = process.env['NODE_LOG_LEVEL'] || 'debug'
+log.level = process.env['NODE_LOG_LEVEL'] || 'info'
 
 let categorySplitString = '###'
 
 var github = new GitHubApi({
-    // optional
-    debug: true,
-    protocol: "https",
-    host: "api.github.com", // should be api.github.com for GitHub
-    // pathPrefix: "/api/v3", // for some GHEs; none for GitHub
-    headers: {
-        "user-agent": "awesome-repodata" // GitHub is happy with a unique user agent
-    },
-    // Promise: require('bluebird'),
-    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
-    timeout: 5000
-});
+  debug: true,
+  protocol: 'https',
+  host: 'api.github.com',
+  headers: { 'User-Agent': 'sandalsoft/awesome-repodata' },
+  // Promise: require('bluebird'),
+  // followRedirects: false, // default: true there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+  timeout: 2500
+})
 
-let repoOwner = 'ikesyo'
-let repoName = 'Himotoki'
+// let repoOwner = 'ikesyo'
+// let repoName = 'Himotoki'
 let readmeUrl = 'https://raw.githubusercontent.com/matteocrippa/awesome-swift/master/README.md'
 
 // INSERT COIN
@@ -28,7 +24,7 @@ getReadme(readmeUrl).then(readme => {
   let categoryList = parseReadmeIntoCategories(readme)
   categoryList.map(category => {
     let repos = category.split('\n')
-    let categoryName = repos[0]
+    let categoryName = repos[0].replace(/\W/g, '')
     log.debug('CategoryName: ' + categoryName)
     repos.map(repo => {
       // log.debug(repo)
@@ -39,89 +35,87 @@ getReadme(readmeUrl).then(readme => {
 
       let owner = match[2]
       let repoName = match[3]
-      log.debug(`\t${owner}/${repoName}`)
+
+      getNumStarsForRepo(owner, repoName)
+      .then(numStars => {
+        log.debug(`\t\t\t${owner}/${repoName} has ${numStars}`)
+      })// then
     })// map
   })// map
 })// then
 
 /**
- * 
+ *
  */
 
-function parseReadmeIntoCategories(readme) {
-  return(readme.split(categorySplitString))
+function parseReadmeIntoCategories (readme) {
+  return readme.split(categorySplitString)
 }
 
-function getReadme(readmeUrl) {
-  var self = this
+function getReadme (readmeUrl) {
   return new Promise(function (resolve, reject) {
     request.get(readmeUrl, function (err, resp) {
       if (err) {
-        log.error('Error getting ' + readmeUrl)
+        log.error('Error getting readme from ' + readmeUrl)
         reject()
       }
       // console.log('got resp at : ' + resp.body)
       resolve(resp.body)
-    })//request
-  })//promise
-}//getReadme()
+    })// request
+  })// promise
+}// getReadme()
 
-
-function getNumStarsForRepo(owner, name) {
-  var self = this
+function getNumStarsForRepo (repoOwner, repoName) {
   return new Promise(function (resolve, reject) {
     github.activity.getStargazersForRepo({
       owner: repoOwner,
       repo: repoName
-    }, function(err, response) {
-       if (err) {
-          log.error('Error checking cached sessionId validity')
-          reject()
-        }
-        resolve(res.length)
-    })//github.
-  })//promise
-}//getNumStarsForRepo()
+    }, function (err, response) {
+      if (err) {
+        log.error(`Error getting stars for repo ${repoOwner}/${repoName}`)
+        reject()
+      }
+      resolve(response.length)
+    })// github.
+  })// promise
+}// getNumStarsForRepo()
 
+// Blatantly copy-pasta'd from http://machinesaredigging.com/2014/04/27/binary-insert-how-to-keep-an-array-sorted-as-you-insert-data-in-it/
+function binaryInsert (value, array, startVal, endVal) {
 
+  var length = array.length
+  var start = typeof startVal !== 'undefined' ? startVal : 0
+  var end = typeof endVal !== 'undefined' ? endVal : length - 1 // !! endVal could be 0 don't use || syntax
+  var m = start + Math.floor((end - start) / 2)
 
+  if (length === 0) {
+    array.push(value)
+    return
+  }
 
+  if (value > array[end]) {
+    array.splice(end + 1, 0, value)
+    return
+  }
 
-function binaryInsert(value, array, startVal, endVal){
+  if (value < array[start]) {
+    array.splice(start, 0, value)
+    return
+  }
 
-	var length = array.length;
-	var start = typeof(startVal) != 'undefined' ? startVal : 0;
-	var end = typeof(endVal) != 'undefined' ? endVal : length - 1;//!! endVal could be 0 don't use || syntax
-	var m = start + Math.floor((end - start)/2);
-	
-	if(length == 0){
-		array.push(value);
-		return;
-	}
+  if (start >= end) {
+    return
+  }
 
-	if(value > array[end]){
-		array.splice(end + 1, 0, value);
-		return;
-	}
+  if (value < array[m]) {
+    binaryInsert(value, array, start, m - 1)
+    return
+  }
 
-	if(value < array[start]){//!!
-		array.splice(start, 0, value);
-		return;
-	}
+  if (value > array[m]) {
+    binaryInsert(value, array, m + 1, end)
+    return
+  }
 
-	if(start >= end){
-		return;
-	}
-
-	if(value < array[m]){
-		binaryInsert(value, array, start, m - 1);
-		return;
-	}
-
-	if(value > array[m]){
-		binaryInsert(value, array, m + 1, end);
-		return;
-	}
-
-	//we don't insert duplicates
+  // we don't insert duplicates
 }
---
+++ test/fixtures/readme_fixture.md
@@ -0,0 +1,49 @@
+# Awesome Swift
+ 
+<!-- 
+
+PLEASE DO NOT UPDATE THIS FILE, UPDATE CONTENTS.JSON INSTEAD. THANK YOU :-)
+
+ -->
+
+
+
+| iOS App | Awesome | Linux | Projects | Updated
+| :-: | :-: | :-: | :-: | :-:
+| [![Download on the App Store](https://img.shields.io/badge/download-app%20store-pink.svg)](https://itunes.apple.com/us/app/awesome-for-swift-cheatsheet/id1078115427) | [![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome) |  :penguin: | ![](https://img.shields.io/badge/swift%20projects-716-orange.svg) | December 07, 2016
+
+### Contributing
+
+Please take a quick look at the [contribution guidelines](.github/CONTRIBUTING.md) first. If you see a package or project here that is no longer maintained or is not a good fit, please submit a pull request to improve this file. Thank you to all [contributors](https://github.com/matteocrippa/awesome-swift/graphs/contributors); you rock!
+
+### Contents
+
+- [Guides](#guides)
+  - [Official Guides](#official-guides)
+  - [Style Guides](#style-guides)
+  - [Third party Guides](#third-party-guides)
+- [Libs](#libs)
+  - [Audio](#audio)
+
+
+## Guides
+*An awesome list of Swift related guides.* 
+
+### Official Guides
+[back to top](#readme) 
+
+* [API Design Guidelines](https://swift.org/documentation/api-design-guidelines/) - Official Swift API design guidelines.
+* [Apple eBook](https://itunes.apple.com/us/book/swift-programming-language/id881256329?mt=11) - Official Apple eBook for swift beginners.
+
+### Style Guides
+[back to top](#readme) 
+
+* [GitHub](https://github.com/github/swift-style-guide) - Style guide & coding conventions for Swift projects.
+
+
+
+### Audio
+*Libs to work with audio* [back to top](#readme) 
+
+* [AudioKit](https://github.com/audiokit/AudioKit) - Powerful audio synthesis, processing and analysis, *without* the steep learning curve.
+
